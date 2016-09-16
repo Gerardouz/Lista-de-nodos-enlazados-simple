faa20b844ad25426a86a6baa49274ab90179fc64
Gerardouz <gerardouz13@gmail.com>
2016-09-16 14:33:50 -0400

    Update pos_actual.py

--- pos_actual.py
+++ pos_actual.py
@@ -3,15 +3,15 @@
 # Metodo para ubicar en actual en la posicion "pos" (enviada por parametro)
 def pos_actual(self, pos):
 		
-		if (pos > self.__n):
-			return
+	if (pos > self.__n):
+		return
 
-		nodo = self.__primero
-		p = 0
-		while (nodo != None):
-			if (p == pos):
-				self.__actual = nodo
-				self.__pos = p
-				return
+	nodo = self.__primero
+	p = 0
+	while (nodo != None):
+		if (p == pos):
+			self.__actual = nodo
+			self.__pos = p
+			return
 		p = p + 1
 		nodo = nodo.sig
