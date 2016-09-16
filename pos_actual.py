245ca63e4d10b52f7fa698789e1ee661b7f4ac9a
Gerardouz <gerardouz13@gmail.com>
2016-09-16 14:12:21 -0400

    Create pos_actual.py

---
+++ pos_actual.py
@@ -0,0 +1,17 @@
+# ver en repl.it: https://repl.it/X3G/3975
+
+
+def pos_actual(self, pos):
+		
+		if (pos > self.__n):
+			return
+
+		nodo = self.__primero
+		p = 0
+		while (nodo != None):
+			if (p == pos):
+				self.__actual = nodo
+				self.__pos = p
+				return
+		p = p + 1
+		nodo = nodo.sig
