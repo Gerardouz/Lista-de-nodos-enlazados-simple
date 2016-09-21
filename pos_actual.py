7331ef3820b4ea80b43f64aae1a786d23f1f2404
Gerardouz <gerardouz13@gmail.com>
2016-09-20 21:12:45 -0400

    Update pos_actual.py

--- pos_actual.py
+++ pos_actual.py
@@ -1,17 +1,89 @@
 # ver en repl.it: https://repl.it/X3G/3975
 
-# Metodo para ubicar en actual en la posicion "pos" (enviada por parametro)
-def pos_actual(self, pos):
-		
-	if (pos > self.__n):
-		return
+class Nodo:
+	def __init__ (self, valor):
+		self.info = valor
+		self.sig = None
+	
+# CLASE LISTA
+class Lista:
+	
+	# CONSTRUCTOR
+	def __init__ (self):
+		self.__primero = None
+		self.__ultimo = None
+		self.__actual = None
+		self.__n = 0
+		self.__pos = 0
 
-	nodo = self.__primero
-	p = 0
-	while (nodo != None):
-		if (p == pos):
-			self.__actual = nodo
-			self.__pos = p
+    # Metodo para insertar al inicio de la lista
+	def insertar_inicio (self, valor):
+		nodo = Nodo (valor)
+		
+		nodo.sig = self.__primero
+		self.__primero = nodo
+		self.__actual = nodo
+		if (self.__ultimo == None):
+			self.__ultimo = nodo
+		
+		self.__n = self.__n+1
+		self.__pos = 0
+		
+	# Metodo para insertar al final de la lista
+	def insertar_ultimo (self, valor):
+		nodo = Nodo(valor)
+		
+		if (self.__ultimo == None):
+			self.__primero = nodo
+		else:
+			self.__ultimo.sig = nodo
+
+		self.__ultimo = nodo
+		self.__actual = nodo
+		self.__n = self.__n + 1
+		self.__pos = self.__n - 1
+		
+	# Metodo para insertar adelanta de la posicion actual de la lista
+	def insertar_actual (self, valor):
+
+		if(self.__n == 0):
+			self.insertar_inicio (valor)
 			return
-		p = p + 1
-		nodo = nodo.sig
+			
+		if(self.__actual == self.__ultimo):
+			self.insertar_ultimo (valor)
+			return
+			
+		nodo = Nodo(valor)
+		nodo.sig = self.__actual.sig
+
+		self.__actual.sig = nodo
+		self.__actual = nodo
+		
+		self.__n = self.__n + 1
+		self.__pos = self.__pos + 1
+	def mostrar (self):
+		
+		nodo = self.__primero
+		for i in range (self.__n):
+			if (i == self.__pos):
+				print "(",nodo.info,")", 
+			else: 
+				print nodo.info,
+			nodo=nodo.sig 
+
+	# Metodo para ubicar en actual en la posicion "pos" (enviada por parametro)
+	def pos_actual(self, pos):
+		
+		if (pos > self.__n):
+			return
+
+		nodo = self.__primero
+		p = 0
+		while (nodo != None):
+			if (p == pos):
+				self.__actual = nodo
+				self.__pos = p
+				return
+			p = p + 1
+			nodo = nodo.sig
