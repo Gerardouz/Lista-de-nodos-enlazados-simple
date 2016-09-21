c3419aa5ed50f3e5111608a1d7da69100935486d
Gerardouz <gerardouz13@gmail.com>
2016-09-20 21:05:21 -0400

    Update busca_valor.py

--- busca_valor.py
+++ busca_valor.py
@@ -1,7 +1,90 @@
 # ver en repl.it: https://repl.it/X3G/3976
 
-# Metodo que busca si un valor existe en la lista 
-def busca_valor (self, valor):
+# CLASE NODO
+class Nodo:
+	def __init__ (self, valor):
+		self.info = valor
+		self.sig = None
+	
+# CLASE LISTA
+class Lista:
+	
+	# CONSTRUCTOR
+	def __init__ (self):
+		self.__primero = None
+		self.__ultimo = None
+		self.__actual = None
+		self.__n = 0
+		self.__pos = 0
+
+    # Metodo para insertar al inicio de la lista
+	def insertar_inicio (self, valor):
+		nodo = Nodo (valor)
+		
+		nodo.sig = self.__primero
+		self.__primero = nodo
+		self.__actual = nodo
+		if (self.__ultimo == None):
+			self.__ultimo = nodo
+		
+		self.__n = self.__n+1
+		self.__pos = 0
+		
+	# Metodo para insertar al final de la lista
+	def insertar_ultimo (self, valor):
+		nodo = Nodo(valor)
+		
+		if (self.__ultimo == None):
+			self.__primero = nodo
+		else:
+			self.__ultimo.sig = nodo
+
+		self.__ultimo = nodo
+		self.__actual = nodo
+		self.__n = self.__n + 1
+		self.__pos = self.__n - 1
+		
+	# Metodo para insertar adelanta de la posicion actual de la lista
+	def insertar_actual (self, valor):
+
+		if(self.__n == 0):
+			self.insertar_inicio (valor)
+			return
+			
+		if(self.__actual == self.__ultimo):
+			self.insertar_ultimo (valor)
+			return
+			
+		nodo = Nodo(valor)
+		nodo.sig = self.__actual.sig
+
+		self.__actual.sig = nodo
+		self.__actual = nodo
+		
+		self.__n = self.__n + 1
+		self.__pos = self.__pos + 1
+
+			
+	# Metodo para vaciar la lista 
+	def vacia_lista(self):
+		
+		num = self.__n
+		for i in range(num):
+			self.elimina_primero()
+			
+	# Metodo para mostrar los elementos de la lista 
+	def mostrar (self):
+		
+		nodo = self.__primero
+		for i in range (self.__n):
+			if (i == self.__pos):
+				print "(",nodo.info,")", 
+			else: 
+				print nodo.info, 
+			nodo=nodo.sig	
+			
+	# Metodo que busca si un valor existe en la lista 
+	def busca_valor (self, valor):
 		
 		nodo  = self.__primero
 		
